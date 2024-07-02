0b2917dd60152f3086abc285afddffa37f7ef882
HOTHEAD01TH <hothead01th@gmail.com>
2024-07-02 17:13:14 +0530

    made website home page, added some animations and did some designing

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,39 @@
-import { Switch, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Services from './pages/Services';
-import Contact from './pages/Contact';
-import AboutUs from './pages/AboutUs';
+// eslint-disable-next-line no-unused-vars
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-function App() {
+// Component import
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+// import BrandsLogo from "./components/BrandsLogo/BrandsLogo.jsx";
+import Services from "./components/Services/Services";
+
+// import BlogsComp from "./components/Blogs/BlogsComp.jsx";
+import Footer from "./components/Footer/Footer";
+
+const App = () => {
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
+
   return (
-    <div className="App">
+    <div className="bg-white dark:bg-black dark:text-white text-black overflow-x-hidden">
       <Navbar />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/Services" component={Services} />
-        <Route path="/Contact" component={Contact} />
-        <Route path="/AboutUs" component={AboutUs} />
-      </Switch>
+      <Hero />
+      {/* <BrandsLogo /> */}
+      <Services />
+      
+      {/* <BlogsComp /> */}
       <Footer />
     </div>
   );
-}
+};
+
 export default App;
